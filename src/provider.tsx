a396fe1e9817831d51188881c119853460f7b302
Hajavololona Armellin <armelgeek5@gmail.com>
2025-06-04 10:32:11 +0300

    fix: disable tanstack debugger

--- src/provider.tsx
+++ src/provider.tsx
@@ -1,6 +1,5 @@
 import { PropsWithChildren } from "react";
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 const queryClient = new QueryClient()
 
 import { LangProvider } from "./services/languages/provider";
@@ -9,7 +8,6 @@ export function Provider({ children }: PropsWithChildren) {
 	return (<LangProvider>
 		<QueryClientProvider client={queryClient} >
 			{children}
-		<ReactQueryDevtools initialIsOpen={false}/>
 		</QueryClientProvider>
 		
 		</LangProvider>);
