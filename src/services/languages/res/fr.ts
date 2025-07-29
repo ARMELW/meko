beb4af6621d7c44890c590bd8e8c11be90142660
Hajavololona Armellin <armelgeek5@gmail.com>
2025-07-29 09:51:51 +0300

    chore: update fr traduction

--- src/services/languages/res/fr.ts
+++ src/services/languages/res/fr.ts
@@ -48,31 +48,28 @@ const fr = {
 	onboarding: {
 		welcome: {
 			title: "Bienvenue sur Meko Academy {name} !",
-			description: "Moi, c'est Fifou, ton compagnon d'aventure. Ensemble, on va explorer un monde rempli de défis amusants et apprendre plein de choses tout en s'amusant ! Prêt(e) à commencer ?"
+			description: "Je suis Fifou, ton compagnon d'aventure. Ensemble, nous allons explorer un monde rempli de défis amusants et apprendre plein de choses tout en s'amusant ! Prêt à commencer ?"
 		},
 		profile: {
 			title: "Choisis ton profil",
 			noChildren: "Vous n'avez pas encore ajouté d'enfant à votre compte."
 		},
 		avatar: {
-	landing: {
-		heroTitle: "Apprendre les maths en s'amusant !",
-		heroDescription: "Avec Meko Academy, l'apprentissage des maths devient un jeu ! Grâce à une interface immersive et des défis interactifs, votre enfant progresse en addition, soustraction, multiplication et division tout en s'amusant.",
-		discover: 'DÉCOUVREZ MEKO ACADEMY EN ACTION !',
-		planTitle: "CHOISISSEZ L'ABONNEMENT QUI VOUS CONVIENT",
-		mensual: 'Mensuel',
-		annual: 'Annuel',
-		switchPlan: 'Changer de cycle de facturation',
-		buyPlan: "Acheter l'offre"
-	},
-			title: "Création compte enfant",
-			subtitle: "Renseignez les informations concernant l'enfant",
-			firstName: "Nom",
-			lastName: "Prénom",
+			title: "Choisis ton avatar",
+			description: "Commence par choisir ton avatar. Tu pourras toujours le changer plus tard si tu veux.",
+			noAvatars: "Aucun avatar disponible pour le moment.",
+			success: "Avatar sélectionné avec succès",
+			successDescription: "Tu peux le modifier à tout moment dans les paramètres de ton profil."
+		},
+		createChild: {
+			title: "Créer un compte enfant",
+			subtitle: "Entrez les informations de l'enfant",
+			firstName: "Prénom",
+			lastName: "Nom",
 			birthday: "Date de naissance",
 			placeholders: {
-				firstName: "Entrez le nom",
-				lastName: "Entrez le prénom"
+				firstName: "Entrez le prénom",
+				lastName: "Entrez le nom"
 			},
 			submit: "Créer le compte"
 		}
@@ -125,46 +122,46 @@ const fr = {
 		}
 	},
 	auth: {
-      login: 'Connexion',
-      success: 'Un code de connexion a été envoyé à votre adresse email',
-      createAccount: 'Création compte parent',
-      provideInfo: 'Renseignez vos informations',
-      firstName: 'Nom',
-      lastName: 'Prénom',
-      email: 'Email',
-      termsAgreement: 'En créant un compte, vous acceptez les',
-      termsLink: 'conditions générales',
-      connect: 'Se connecter',
-	  signUp: 'Créer un compte',
-      alreadyHaveAccount: 'Vous avez déjà un compte ?',
-      verification: {
-         success: {
-            signup: 'Compte créé avec succès !',
-            login: 'Connexion réussie !',
-            description: 'Bienvenue sur Meko Academy'
-         }
-      },
-      errors: {
-         email: {
-            required: 'L\'email est requis',
-            invalid: 'Format d\'email invalide',
-            alreadyExists: 'Cet email est déjà utilisé. Connectez-vous à la place.'
-         },
-         firstName: {
-            required: 'Le nom est requis'
-         },
-         lastName: {
-            required: 'Le prénom est requis'
-         },
-         otp: {
-            required: 'Le code de vérification doit contenir 6 chiffres',
-            length: 'Le code de vérification doit contenir 6 chiffres',
-            invalid: 'Le code de vérification doit contenir uniquement des chiffres'
-         }
-      },
-	  logout: 'Déconnexion',
-	  loggingOut: 'Déconnexion ...'
-   },
+		login: 'Connexion',
+		success: 'Un code de connexion a été envoyé à votre adresse email',
+		createAccount: 'Création compte parent',
+		provideInfo: 'Renseignez vos informations',
+		firstName: 'Nom',
+		lastName: 'Prénom',
+		email: 'Email',
+		termsAgreement: 'En créant un compte, vous acceptez les',
+		termsLink: 'conditions générales',
+		connect: 'Se connecter',
+		signUp: 'Créer un compte',
+		alreadyHaveAccount: 'Vous avez déjà un compte ?',
+		verification: {
+			success: {
+				signup: 'Compte créé avec succès !',
+				login: 'Connexion réussie !',
+				description: 'Bienvenue sur Meko Academy'
+			}
+		},
+		errors: {
+			email: {
+				required: 'L\'email est requis',
+				invalid: 'Format d\'email invalide',
+				alreadyExists: 'Cet email est déjà utilisé. Connectez-vous à la place.'
+			},
+			firstName: {
+				required: 'Le nom est requis'
+			},
+			lastName: {
+				required: 'Le prénom est requis'
+			},
+			otp: {
+				required: 'Le code de vérification doit contenir 6 chiffres',
+				length: 'Le code de vérification doit contenir 6 chiffres',
+				invalid: 'Le code de vérification doit contenir uniquement des chiffres'
+			}
+		},
+		logout: 'Déconnexion',
+		loggingOut: 'Déconnexion ...'
+	},
 	landing: {
 		heroTitle: "Apprendre les maths en s'amusant !",
 		heroDescription: "Avec Meko Academy, l'apprentissage des maths devient un jeu ! Grâce à une interface immersive et des défis interactifs, votre enfant progresse en addition, soustraction, multiplication et division tout en s'amusant.",
