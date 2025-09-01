e744a197a7768cb66c9840dc43fd2d3935956d69
Hajavololona Armellin <armelgeek5@gmail.com>
2025-09-01 11:54:18 +0300

    fix: vite config

--- vite.config.ts
+++ vite.config.ts
@@ -17,12 +17,9 @@ export default defineConfig({
 		"process.env.VITE_APP_SERVER_URL": JSON.stringify(process.env.VITE_APP_SERVER_URL)
 	},
 	server: {
-		host: '0.0.0.0',
-		port: 4173,
 		allowedHosts: ['dev.meko.ac'],
 	},
 	preview: {
-		port: 4173,
 		allowedHosts: ['dev.meko.ac'],
 	},
 });
